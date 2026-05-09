ec8351378a7c58cb823fa59b0a62724fac5de6a6
Emilio <cg22073@ues.edu.sv>
2026-05-08 21:21:26 -0600

    creacion de rutas de crud para carreras

--- src/main.rs
+++ src/main.rs
@@ -10,34 +10,40 @@ use sqlx::{FromRow, PgPool};
 mod models{
     pub mod profesor_model;
     pub mod materias_model;
+    pub mod carreras_model;
 }
 mod controllers{
     pub mod profesor_comtroller;
     pub mod materias_controller;
+    pub mod carreras_controller;
 }
 mod services{
     pub mod profesor_services;
     pub mod materias_services;
+    pub mod carreras_services;
 }
 
 mod repository{
     pub mod materias_repository;
+    pub mod carreras_repository;
 }
-mod routes;
+pub mod routes;
 
 #[tokio::main]
 async fn main() {
 
     let db = PgPool::connect(
         //"postgresql://postgres.vdokkavvfsctthvhbvlb:proyecto123@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true"
-        "postgresql://postgres.xunyklqfgevolgprpmpb:e5wT9nlrBqToWYig@aws-1-us-west-2.pooler.supabase.com:5432/postgres?sslmode=require"
+       "postgresql://postgres.xunyklqfgevolgprpmpb:e5wT9nlrBqToWYig@aws-1-us-west-2.pooler.supabase.com:5432/postgres?sslmode=require"
+       // "postgresql://postgres.rwxsbbflnarrfxjcxvpw:Diosmeama1516@@aws-1-us-east-2.pooler.supabase.com:6543/postgres"
+
     )
-    .await
-    .unwrap();
+        .await
+        .unwrap();
     println!("Base de datos conectada");
 
     let app = routes::crear_rutas(db);
-        
+
 
     let mostrar = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
--- src/routes.rs
+++ src/routes.rs
@@ -1,12 +1,14 @@
 use axum::{
-    routing::{get, post},
+    routing::{get, post, put, delete},
     Router,
 };
-
 use sqlx::PgPool;
-
+use crate::controllers::carreras_controller::{
+    crear_carrera, listar_carreras, obtener_carrera, actualizar_carrera, eliminar_carrera,
+};
 use crate::controllers::profesor_comtroller::obtener_Profesores;
 use crate::controllers::materias_controller::{obtener_materias, crear_materia};
+
 
 pub fn crear_rutas(
     db: PgPool,
@@ -20,6 +22,18 @@ pub fn crear_rutas(
         .route(
             "/materias",
             get(obtener_materias).post(crear_materia),
+        )
+        .route(
+            "/carreras",
+            post(crear_carrera).get(listar_carreras)
+        )
+        .route(
+            "/carreras/{id}",
+            get(obtener_carrera)
+        )
+        .route( "/carreras/actualizar/{id}", put(actualizar_carrera)
+        )
+        .route( "/carreras/eliminar/{id}", delete(eliminar_carrera)
         )
         .with_state(db)
 }
