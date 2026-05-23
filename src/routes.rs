ab47b20ec2baa88d01c288473737c5cbd1ef4715
Emilio <cg22073@ues.edu.sv>
2026-05-23 13:59:57 -0600

    Se ajusto lineas de codigo de routes en carrera y estudiantes

--- src/routes.rs
+++ src/routes.rs
@@ -4,22 +4,18 @@ use axum::{
 };
 use sqlx::PgPool;
 use crate::controllers::estudiantes_controller::{
-    crear_estudiante,actualizar_estudiante,obtener_estudiantes,obtener_estudiante_por_id,eliminar_estudiante
-};
+    crear_estudiante,actualizar_estudiante,obtener_estudiantes,obtener_estudiante_por_id,eliminar_estudiante};
 use crate::controllers::carreras_controller::{
-    crear_carrera, listar_carreras, obtener_carrera, actualizar_carrera, eliminar_carrera,
-};
+    crear_carrera, listar_carreras, obtener_carrera, actualizar_carrera, eliminar_carrera, };
 use crate::controllers::profesor_controller::{obtener_profesor,crear_profesor
     ,actualizar_profesor,eliminar_profesor,obtener_profesor_por_id};
 use crate::controllers::inscripciones_controller::{actualizar_inscripcion, crear_inscripcion, eliminar_inscripcion, listar_inscripciones, obtener_inscripcion};
 use crate::controllers::materias_controller::{
     listar_materias, crear_materia, obtener_materia, actualizar_materia,eliminar_materia};
 
-
 pub fn crear_rutas(
     db: PgPool,
 ) -> Router {
-
     Router::new()
         .route(
             "/profesores",
